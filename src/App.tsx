490ac56677f4afff8cf1275551300d0a78de4073
Alonso <alonso.rapado@hotmail.com>
2021-11-22 01:35:08 +0100

    trackpad done

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {
-  BrowserRouter, Route, Switch
+  BrowserRouter, Route, Switch, useLocation
 } from "react-router-dom";
 import InitPage from './InitPage/InitPage';
 import Results from './Results/Results';
 import Trackpad from './Trackpad/Trackpad';
-import { AnimatePresence } from 'framer-motion';
 import { Grid } from '@mui/material';
 
 class App extends Component {
-
+  currentCoin = "lb";
   state = {
     amount: 0
-  }
+  };
 
   handleCallback = (amount: number) => {
     this.setState({ amount: amount });
@@ -24,19 +23,17 @@ class App extends Component {
     return (
       <Grid container className="defaultContainer">
         <BrowserRouter>
-          <AnimatePresence exitBeforeEnter initial={false}>
-            <Switch>
-              <Route exact path="/">
-                <InitPage />
-              </Route>
-              <Route path="/trackpad" >
-                <Trackpad parentCallback={this.handleCallback} />
-              </Route>
-              <Route path="/results" >
-                <Results amount={this.state.amount} />
-              </Route>
-            </Switch>
-          </AnimatePresence>
+          <Switch>
+            <Route exact path="/">
+              <InitPage />
+            </Route>
+            <Route path="/trackpad" >
+              <Trackpad parentCallback={this.handleCallback} currentCoin ={this.currentCoin}/>
+            </Route>
+            <Route path="/results" >
+              <Results amount={this.state.amount} currentCoin ={this.currentCoin}/>
+            </Route>
+          </Switch>
         </BrowserRouter>
       </Grid>
     );
--- src/Trackpad/Trackpad.tsx
+++ src/Trackpad/Trackpad.tsx
@@ -1,5 +1,5 @@
 import { ArrowBack } from "@mui/icons-material";
-import { Box, Button, FormControl, Grid, Input, InputAdornment, InputLabel } from "@mui/material";
+import { Box, Button, Container, FormControl, Grid, Input, InputAdornment, InputLabel } from "@mui/material";
 import React from "react";
 import { Component } from "react";
 import { Link } from "react-router-dom";
@@ -13,10 +13,15 @@ export default class Trackpad extends Component<any, any> {
         const buttons: any = [];
         const grid: any = [];
         let coin = require('../config/config.json');
-        let currentCoin = "lb";
 
-        for (let i = 1; i <= 9; i++) {
-            buttons.push(<Button variant="outlined" className="inputButton" value={i} onClick={() => addValue(i)}><p>{i}</p></Button>)
+        const buttonGenerator = () => {
+            for (let i = 1; i <= 9; i++) {
+                buttons.push(
+                    <Button variant="outlined" className="inputButton" value={i} onClick={() => addValue(i)}>
+                        <p>{i}</p>
+                    </Button>
+                )
+            }
         }
         const updateValue = () => {
             let newValue: any = this.state.arrayValue.join("");
@@ -57,64 +62,78 @@ export default class Trackpad extends Component<any, any> {
         function FormRow(_index: number) {
             return (
                 <React.Fragment>
-                    <Grid item xs={1}>
+                    <Grid item >
                         {buttons[_index]}
                     </Grid>
-                    <Grid item xs={1}>
+                    <Grid item >
                         {buttons[_index + 1]}
                     </Grid>
-                    <Grid item xs={1}>
+                    <Grid item >
                         {buttons[_index + 2]}
                     </Grid>
                 </React.Fragment>
             );
         }
-
-        for (let i = 0; i < 3; i++) {
-            grid.push(<Grid container item spacing={2} className="buttonRow">
-                {FormRow(i * 3)}
-            </Grid>)
+        const gridGenerator = () => {
+            buttonGenerator()
+            for (let i = 0; i < 3; i++) {
+                grid.push(<Grid container item className="buttonRow">
+                    {FormRow(i * 3)}
+                </Grid>)
+            }
         }
 
-
+        gridGenerator();
         return (
             <Grid container className="semiContainer">
-                <Grid container className="insideCenterContainer">
-                    <FormControl fullWidth sx={{ m: 1 }} variant="standard">
-                        <InputLabel htmlFor="standard-adornment-amount" className="trackpadLabel"><p>Select amount</p></InputLabel>
-                        <Input
-                            id="standard-adornment-amount"
-                            type="number"
-                            disableUnderline={true}
-                            readOnly={true}
-                            className="trackpadInput"
-                            value={this.state.finalValue}
-                            startAdornment={<InputAdornment position="start" className="inputAdornment"><p>{coin[currentCoin].currency}</p></InputAdornment>}
-                        />
-                        <Box sx={{ flexGrow: 1 }} className="trackpad">
-                            <Grid container spacing={2}>
-                                {grid}
-                                <Grid container item spacing={2} className="buttonRow">
-                                    <Grid item xs={1}>
-                                        <Button variant="outlined" className="inputButton" onClick={() => removeValue()}><ArrowBack /></Button>
-                                    </Grid>
-                                    <Grid item xs={1}>
-                                        <Button variant="outlined" className="inputButton" value={0} onClick={() => addValue(0)}><p>0</p></Button>
-                                    </Grid>
-                                    <Grid item xs={1}></Grid>
+                <FormControl sx={{ m: 1 }} variant="standard">
+                    <InputLabel htmlFor="standard-adornment-amount" className="trackpadLabel">
+                        <p>Select amount</p>
+                    </InputLabel>
+                    <Input
+                        id="standard-adornment-amount"
+                        type="number"
+                        disableUnderline={true}
+                        readOnly={true}
+                        className="trackpadInput"
+                        value={this.state.finalValue}
+                        startAdornment={<InputAdornment position="start" className="inputAdornment"><p>{coin[this.props.currentCoin].currency}</p></InputAdornment>}
+                    />
+                    <Box className="trackpad">
+                        <Grid container xs={8}>
+                            {grid}
+                            <Grid container item className="buttonRow">
+                                <Grid item >
+                                    <Button variant="outlined" className="inputButton" onClick={() => removeValue()}>
+                                        <ArrowBack className="backArrow" />
+                                    </Button>
+                                </Grid>
+                                <Grid item >
+                                    <Button variant="outlined" className="inputButton" value={0} onClick={() => addValue(0)}>
+                                        <p>0</p>
+                                    </Button>
+                                </Grid>
+                                <Grid item >
+                                    <Button variant="outlined" className="inputButton" style={{ visibility: "hidden" }} >
+                                        <p>0</p>
+                                    </Button>
                                 </Grid>
                             </Grid>
-                        </Box>
-                        {this.state.finalValue != 0 ? 
-                            <Link to="/results">
-                                <Button type="submit" variant="outlined" className="submitButton" onClick={() => onTrigger()}><p>submit</p></Button>
-                            </Link>
-                            :
-                            <Button type="submit" variant="outlined" className="submitButton" disabled={true}><p>submit</p></Button>
-                        }
+                        </Grid>
+                    </Box>
+                    {this.state.finalValue != 0 ?
+                        <Link to="/results" style={{ textDecoration: "none" }}>
+                            <Button variant="outlined" className="submitButton" onClick={() => onTrigger()}>
+                                <p>Submit</p>
+                            </Button>
+                        </Link>
+                        :
+                        <Button variant="outlined" className="submitButton" disabled={true}>
+                            <p>Submit</p>
+                        </Button>
+                    }
 
-                    </FormControl>
-                </Grid>
+                </FormControl>
             </Grid>
         )
     }
